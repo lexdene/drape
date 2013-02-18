29fbb856b14855c0822a8afd6453e094118eb65d
lexdene <lexdene@sohu.com>
2013-02-18 19:21:48 +0800

    add debug/sql

--- debug.py
+++ debug.py
@@ -3,3 +3,7 @@ import application
 def debug(data):
 	a = application.Application.singleton()
 	a.log('debug',data)
+
+def sql(data):
+	a = application.Application.singleton()
+	a.log('debug','[sql] '+data)
