85e3bcfd975ac90d44690104b5d7b373ff0d90ff
Elephant Liu <lexdene@sohu.com>
2014-02-08 20:39:41 +0800

    rebuild validate

--- validate.py
+++ validate.py
@@ -2,50 +2,68 @@
 
 import re
 
-def validate_params(params,validateList):
-	for validateItem in validateList:
-		key = validateItem['key']
-		name = validateItem['name']
-		value = params.get(key)
-		for validate in validateItem['validates']:
-			method = validate[0]
-			if 'notempty' == method:
-				if value is None:
-					return dict(
-						result = False,
-						msg = u'%s不能为空'%name
-					)
-			elif 'len' == method:
-				if len(value) < validate[1] or len(value) > validate[2]:
-					return dict(
-						result = False,
-						msg = u'%s的长度必须在[%d,%d]之间'%(name,validate[1],validate[2])
-					)
-			elif 'equal' == method:
-				if value != params[validate[1]]:
-					return dict(
-						result = False,
-						msg = u'%s必须要和%s相同'%(name,validate[2])
-					)
-			elif 'email' == method:
-				re_email = r'^[-a-zA-Z0-9_]*@[-.a-zA-Z0-9_]*$'
-				reg = re.compile(re_email)
-				if not reg.match(value):
-					return dict(
-						result = False,
-						msg = u'%s不符合E-mail格式'%(name)
-					)
-			elif 'int' == method:
-				re_num = r'^-?[0-9]*$'
-				reg = re.compile(re_num)
-				if not reg.match(str(value)):
-					return dict(
-						result = False,
-						msg = u'%s不符合整数格式'%(name)
-					)
-			else:
-				raise KeyError(method)
-	return dict(
-		result = True,
-		msg = ''
-	)
+
+def validate_params(params, validators):
+    validate_results = dict()
+    for key, item in validators.iteritems():
+        field = _Field()
+        field.value = params.get(key)
+        field.params = params
+
+        for validate in item['validates']:
+            method_name, args = validate[0], validate[1:]
+            method_func = getattr(
+                field,
+                'validate_%s' % method_name
+            )
+
+            # 如果验证通过，返回True，否则返回False
+            result = method_func(*args)
+
+            if not result:
+                if key not in validate_results:
+                    validate_results[key] = []
+
+                validate_results[key].append(validate)
+
+    return validate_results
+
+
+class _Field(object):
+    def validate_notempty(self):
+        if self.value is None or self.value == '':
+            return False
+        else:
+            return True
+
+    def validate_len(self, minlen, maxlen):
+        if len(self.value) < minlen or len(self.value) > maxlen:
+            return False
+        else:
+            return True
+
+    def validate_equal(self, to_key):
+        if self.value != self.params[to_key]:
+            return False
+        else:
+            return True
+
+    _EMAIL_REG = re.compile(
+        r'^[-a-zA-Z0-9_]*@[-.a-zA-Z0-9_]*$'
+    )
+
+    def validate_email(self):
+        if not self._EMAIL_REG.match(self.value):
+            return False
+        else:
+            return True
+
+    _INT_REG = re.compile(
+        r'^-?[0-9]*$'
+    )
+
+    def validate_int(self):
+        if not self._INT_REG.match(self.value):
+            return False
+        else:
+            return True
