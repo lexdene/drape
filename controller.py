ca8951301c377e5db7f1dd1c1b10008356928520
Elephant Liu <lexdene@sohu.com>
2014-05-26 16:33:06 +0800

    add json_controller decorator

---
+++ controller.py
@@ -0,0 +1,9 @@
+from .response import json_response
+
+def json_controller(func):
+    ' controller that always return json '
+    def response_as_json(request):
+        response_data = func(request)
+        return json_response(response_data)
+
+    return response_as_json
